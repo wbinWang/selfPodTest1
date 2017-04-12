e98c7dea7fece33bf89f744c7a4422050b3d72a3
王彬 <1462999792@qq.com>
2017-04-12 14:47:30 +0800

    new pod test begin
    Signed-off-by: 王彬 <1462999792@qq.com>

---
+++ Example/Pods/Target Support Files/Pods-podSpecTest1_Example/Pods-podSpecTest1_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_podSpecTest1_Example : NSObject
+@end
+@implementation PodsDummy_Pods_podSpecTest1_Example
+@end
---
+++ Example/Pods/Target Support Files/podSpecTest1/podSpecTest1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_podSpecTest1 : NSObject
+@end
+@implementation PodsDummy_podSpecTest1
+@end
