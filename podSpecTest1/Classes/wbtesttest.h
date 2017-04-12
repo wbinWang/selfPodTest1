8b49c9f768e372aa9799d3926c2926da7adef25a
王彬 <1462999792@qq.com>
2017-04-12 15:37:03 +0800

    添加了testtest文件
    Signed-off-by: 王彬 <1462999792@qq.com>

---
+++ podSpecTest1/Classes/wbtesttest.h
@@ -0,0 +1,13 @@
+//
+//  wbtesttest.h
+//  Pods
+//
+//  Created by wenbin on 2017/4/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface wbtesttest : NSObject
+
+@end
---
+++ podSpecTest1/Classes/wbtesttest.m
@@ -0,0 +1,13 @@
+//
+//  wbtesttest.m
+//  Pods
+//
+//  Created by wenbin on 2017/4/12.
+//
+//
+
+#import "wbtesttest.h"
+
+@implementation wbtesttest
+
+@end
